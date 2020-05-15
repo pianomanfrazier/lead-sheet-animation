f2e2bf8b427fe7cdbfb378c84607a1a99d7c5a42
Ryan Frazier <pianomanfrazier@gmail.com>
2020-05-15 13:11:55 -0600

    Add roman analysis

--- src/Main.elm
+++ src/Main.elm
@@ -41,7 +41,7 @@ update msg model =
                 KK.Left ->
                     ( { model | count = model.count - 1, chordCount = 0 }, Cmd.none )
 
-                KK.Backspace ->
+                KK.Delete ->
                     ( { model | count = 0, chordCount = 0 }, Cmd.none )
 
                 KK.D ->
@@ -103,15 +103,35 @@ plainSong =
     , [ Chord "-" "" 1 ]
     ]
 
+plainSongRomans : Song
+plainSongRomans =
+    [ [ Chord "I" "" 1 ]
+    , [ Chord "V7/IV" "" 1 ]
+    , [ Chord "IV" "" 1 ]
+    , [ Chord "I" "" 1 ]
+    , [ Chord "I" "" 1 ]
+    , [ Chord "V7/V" "" 1 ]
+    , [ Chord "V" "7" 1 ]
+    , [ Chord "-" "" 1 ]
+    , [ Chord "I" "" 1 ]
+    , [ Chord "V/IV" "" 1 ]
+    , [ Chord "IV" "" 1 ]
+    , [ Chord "I" "" 1 ]
+    , [ Chord "I" "" 1 ]
+    , [ Chord "V" "7" 1 ]
+    , [ Chord "I" "" 1 ]
+    , [ Chord "-" "" 1 ]
+    ]
+
 
 reharmSong : Song
 reharmSong =
     [ [ Chord "F" "" 1 ]
-    , [ Chord "F" "7" 1 ]
-    , [ Chord "Bb" "" 1
+    , [ Chord "A" "7" 1 ]
+    , [ Chord "Bb" "" 2
       , Chord "C(sus4)" "" 1
       ]
-    , [ Chord "F" "" 1
+    , [ Chord "F" "" 2
       , Chord "A" "7" 1
       ]
     , [ Chord "Dmin" "7" 1 ]
@@ -120,20 +140,57 @@ reharmSong =
     , [ Chord "-" "" 1 ]
     , [ Chord "F" "" 1 ]
     , [ Chord "A" "7" 1 ]
-    , [ Chord "Bb" "" 1
+    , [ Chord "Bb" "" 2
       , Chord "Eb" "7" 1
       ]
-    , [ Chord "F" "" 1 ]
-    , [ Chord "F" "" 1 ]
-    , [ Chord "C" "7" 1 ]
+    , [ Chord "F" "" 2
+      , Chord "A7" "" 1
+      ]
+    , [ Chord "Dmin" "7" 1 ]
+    , [ Chord "Gmin" "7" 2
+      , Chord "C" "7" 1
+      ]
     , [ Chord "F" "" 1 ]
     , [ Chord "C(sus4)" "" 1 ]
+    ]
+
+reharmSongRomans : Song
+reharmSongRomans =
+    [ [ Chord "I" "" 1 ]
+    , [ Chord "V7/vi" "" 1 ]
+    , [ Chord "IV" "" 2
+      , Chord "V(sus4)" "" 1
+      ]
+    , [ Chord "I" "" 2
+      , Chord "V7/vi" "" 1
+      ]
+    , [ Chord "vi" "" 1 ]
+    , [ Chord "V7/V" "" 1 ]
+    , [ Chord "V" "7" 1 ]
+    , [ Chord "-" "" 1 ]
+    , [ Chord "I" "" 1 ]
+    , [ Chord "V7/vi" "7" 1 ]
+    , [ Chord "IV" "" 2
+      , Chord "bVII" "7" 1
+      ]
+    , [ Chord "I" "" 2
+      , Chord "V7/vi" "" 1
+      ]
+    , [ Chord "vi" "" 1 ]
+    , [ Chord "ii" "" 2
+      , Chord "V" "7" 1
+      ]
+    , [ Chord "I" "" 1 ]
+    , [ Chord "V(sus4)" "" 1 ]
     ]
 
 
 song : Song
 song =
+    -- plainSong
+    -- plainSongRomans
     reharmSong
+    -- reharmSongRomans
 
 viewChord : Model -> Int -> Int -> Chord -> Html Msg
 viewChord model measureCount chordCount chord =
