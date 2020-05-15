b7c8ac5e36b1c5a84d925e1de47b59f8e4bbfda6
Ryan Frazier <pianomanfrazier@gmail.com>
2020-05-15 12:19:22 -0600

    Multi chords per measure support

--- src/Main.elm
+++ src/Main.elm
@@ -1,24 +1,26 @@
 module Main exposing (main)
 
+import Array
 import Browser
 import Browser.Events exposing (onKeyDown)
-import Html exposing (Html, h1, text)
+import Html exposing (Html, text)
 import Html.Attributes as Attributes
 import Json.Decode as D
-import Keyboard.Event as KE exposing (KeyboardEvent, decodeKeyboardEvent)
+import Keyboard.Event exposing (KeyboardEvent, decodeKeyboardEvent)
 import Keyboard.Key as KK
 
 
 type alias Model =
-    { chords : List Chord
+    { song : Song
     , count : Int
+    , chordCount : Int
     , disabled : Bool
     }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( Model chords 0 False, Cmd.none )
+    ( Model song 0 0 False, Cmd.none )
 
 
 type Msg
@@ -31,16 +33,16 @@ update msg model =
         HandleKeyboardEvent event ->
             case event.keyCode of
                 KK.Spacebar ->
-                    ( { model | count = model.count + 1 }, Cmd.none )
+                    ( increment model , Cmd.none )
 
                 KK.Right ->
-                    ( { model | count = model.count + 1 }, Cmd.none )
+                    ( increment model , Cmd.none )
 
                 KK.Left ->
-                    ( { model | count = model.count - 1 }, Cmd.none )
+                    ( { model | count = model.count - 1, chordCount = 0 }, Cmd.none )
 
                 KK.Backspace ->
-                    ( { model | count = 0 }, Cmd.none )
+                    ( { model | count = 0, chordCount = 0 }, Cmd.none )
 
                 KK.D ->
                     ( { model | disabled = not model.disabled }, Cmd.none )
@@ -48,6 +50,17 @@ update msg model =
                 -- ignore anything else
                 _ ->
                     ( model, Cmd.none )
+
+increment : Model -> Model
+increment model =
+    let
+        currentMeasure = Array.get (modBy (List.length model.song) model.count) (Array.fromList model.song) |> Maybe.withDefault []
+        chordCount = List.length currentMeasure - 1
+    in
+    if model.chordCount < chordCount then
+        { model | chordCount = model.chordCount + 1 }
+    else
+        { model | count = model.count + 1, chordCount = 0 }
 
 
 subscriptions : Model -> Sub Msg
@@ -58,35 +71,100 @@ subscriptions _ =
 type alias Chord =
     { name : String
     , alt : String
+    , space : Int
     }
 
-chords : List Chord
-chords =
-    [ Chord "C" "7"
-    , Chord "C" "7"
-    , Chord "C" "7"
-    , Chord "C" "7"
-    , Chord "F" "7"
-    , Chord "F" "7"
-    , Chord "C" "7"
-    , Chord "C" "7"
-    , Chord "G" "7"
-    , Chord "F" "7"
-    , Chord "C" "7"
-    , Chord "C" "7"
+
+type alias Measure =
+    List Chord
+
+
+type alias Song =
+    List Measure
+
+
+plainSong : Song
+plainSong =
+    [ [ Chord "F" "" 1 ]
+    , [ Chord "F" "7" 1 ]
+    , [ Chord "Bb" "" 1 ]
+    , [ Chord "F" "" 1 ]
+    , [ Chord "F" "" 1 ]
+    , [ Chord "G" "7" 1 ]
+    , [ Chord "C" "7" 1 ]
+    , [ Chord "-" "" 1 ]
+    , [ Chord "F" "" 1 ]
+    , [ Chord "F" "7" 1 ]
+    , [ Chord "Bb" "" 1 ]
+    , [ Chord "F" "" 1 ]
+    , [ Chord "F" "" 1 ]
+    , [ Chord "C" "7" 1 ]
+    , [ Chord "F" "" 1 ]
+    , [ Chord "-" "" 1 ]
     ]
 
-viewChord : Int -> Bool -> Int -> Chord -> Html Msg
-viewChord count disabled measureNum chord =
+
+reharmSong : Song
+reharmSong =
+    [ [ Chord "F" "" 1 ]
+    , [ Chord "F" "7" 1 ]
+    , [ Chord "Bb" "" 1
+      , Chord "C(sus4)" "" 1
+      ]
+    , [ Chord "F" "" 1
+      , Chord "A" "7" 1
+      ]
+    , [ Chord "Dmin" "7" 1 ]
+    , [ Chord "G" "7" 1 ]
+    , [ Chord "C" "7" 1 ]
+    , [ Chord "-" "" 1 ]
+    , [ Chord "F" "" 1 ]
+    , [ Chord "A" "7" 1 ]
+    , [ Chord "Bb" "" 1
+      , Chord "Eb" "7" 1
+      ]
+    , [ Chord "F" "" 1 ]
+    , [ Chord "F" "" 1 ]
+    , [ Chord "C" "7" 1 ]
+    , [ Chord "F" "" 1 ]
+    , [ Chord "C(sus4)" "" 1 ]
+    ]
+
+
+song : Song
+song =
+    reharmSong
+
+viewChord : Model -> Int -> Int -> Chord -> Html Msg
+viewChord model measureCount chordCount chord =
     Html.div
-        [ Attributes.classList [ ("measure", True), ("highlight", measureNum == modBy (List.length chords) count && not disabled) ] ]
-        [ Html.text chord.name, Html.sup [] [ Html.text chord.alt ]]
+        [ Attributes.classList [("chord", True), ("highlight", modBy (List.length model.song) model.count == measureCount && model.chordCount == chordCount)]
+        , Attributes.attribute "data-weight" (String.fromInt chord.space)
+        , Attributes.attribute "data-chord" (String.fromInt chordCount)
+        ]
+        [ Html.text chord.name, Html.sup [] [ Html.text chord.alt ] ]
+
+
+viewMeasure : Model -> Int -> Measure -> Html Msg
+viewMeasure model measureCount measure =
+    Html.div
+        [ Attributes.class "measure"
+        , Attributes.attribute "data-measure" (String.fromInt measureCount)
+        ]
+        (List.indexedMap (viewChord model measureCount) measure)
+
 
 view : Model -> Html Msg
 view model =
+    let
+        count = modBy (List.length model.song) model.count
+    in
     Html.div
-        [ Attributes.class "grid" ]
-        (List.indexedMap (viewChord model.count model.disabled) model.chords)
+        [ Attributes.class "grid"
+        , Attributes.classList [("disabled", model.disabled)]
+        , Attributes.attribute "data-count" (String.fromInt count)
+        ]
+        (List.indexedMap (viewMeasure model) model.song)
 
 
 main : Program () Model Msg
